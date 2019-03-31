efe85ab8de5e035dceb039e34a9957345314b210
soufiane19745 <45607146+soufiane19745@users.noreply.github.com>
2019-03-31 17:12:20 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -2427,21 +2427,6 @@ var prefix = "-";
        
 });
 
-  client.on('message', async message => {
-  if(message.content.startsWith(prefix + "إقتراحي هو")) {
-  await  message.channel.send(``اكتب اقتراحك الان``)
-    let filter = m => m.author.id === message.author.id
-      var text = '';
-        let sugsa = message.channel.awaitMessages(filter, { max: 1, time: 60000})
-          .then(co => {
-            text = co.first().content
- 
-              message.channel.send(``تم حفظ اقتراحك الرجاء انتضار الرد من قبل الاداره``)
-                client.channels.get("559841530798669859").send(`${message.author.username} إقتراح هو  => ${text}`)
- 
-              })
-            }
-          })
 
 client.on('message', message => {
     if (message.content.startsWith("-bans")) {
