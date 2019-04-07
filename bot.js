5b5e0a099e5d21353dceb43be3d642fff768b669
soufiane19745 <45607146+soufiane19745@users.noreply.github.com>
2019-04-07 18:28:54 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -2068,24 +2068,9 @@ var fkk =[
 });
 
 
-client.on("guildMemberAdd", member => {
-  let welcome = member.guild.channels.find("name","Welcome");
-  if(!welcome) return;
-  if(welcome) {
-      let embed = new Discord.RichEmbed()
-      .setColor("RANDOM")
-      .setThumbnail(member.user.avatarURL)  
-      .setAuthor(member.user.username , member.user.avatarURL)
-      .addField("**Welcome To**", `[${member.guild.name}]`, true)
-      .addField(`**Number**`, `[${member.guild.memberCount}]`, true)
-      .addField("**Name**", `[${member.user.username}#${member.user.discriminator}]`,true)
-      .addField("**ID**", `[${member.user.id}]`, true)
-      .addField('**Created AT**',`${moment(member.user.createdAt).format('D/M/YYYY h:mm a')}**n** `${moment(member.user.createdAt).fromNow()}``,true)            
-      .addField("**Joined At**",`${moment(member.user.joinedAt).format('D/M/YYYY h:mm a')}**n** `${moment(member.user.joinedAt).fromNow()}``,true)    
-      welcome.send(embed)
 
-  }
-  })
+
+
 
 
 
